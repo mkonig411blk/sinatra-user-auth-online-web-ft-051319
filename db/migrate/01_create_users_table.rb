d60d9bd51307030709e7d73bca16ff736d8426a7
Margot Konig <margot.konig@blackrock.com>
2019-07-08 18:58:03 +0000

    Automatically backed up by Learn

---
+++ db/migrate/01_create_users_table.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  validates_presence_of :name, :email, :password
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+    end
+  end
+end
